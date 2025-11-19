193b6210e6c3d46888d67b66131f1a4b55ef7199
swnaguit23-0178-cmd <swnaguit23-0178@cca.edu.ph>
2025-11-19 19:59:52 +0800

    naguit

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
 
 class AuthController extends Controller
 {
@@ -13,10 +17,21 @@ class AuthController extends Controller
         return view('authentication.login');
     }
 
-    function performlogin(){
+    function register(Request $request){
+        $request->validate([
+            'name' =>'required|string|max:255',
+            'email'     =>'required|email|unique:users',
+            'password'  =>'required|min:6|confirmed'
+        ]);
 
+        User::create([
+            'name' => $request->name,
+            'email'     => $request->email,
+            'password'  => Hash::make($request->password)
+        ]);
+        return redirect()->route('login.form')->with('success','Registration successful!');
     }
-    function performregister(){
+    function login(Request $request){
 
     }
 
